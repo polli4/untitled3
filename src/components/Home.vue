8e824826048ad1d4820b8d2e6b8fe7defecdabb0
polli4 <polyarush.iza@gmail.com>
2020-09-08 21:13:45 +0500

    🥴

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="articles">
+    <div class="search">
+      <input>
+    </div>
     <div
         class="first"
         v-for="(article, index) in $store.state.articles"
@@ -21,14 +24,38 @@ export default {
 <style scoped>
 .articles{
   display: flex;
-  background: lightskyblue;
   flex-flow: column;
+  align-items: center;
+  justify-content: space-around;
+}
+.first{
+  display: flex;
+  background: lightskyblue;
   font-family: 'Kufam', cursive;
   color: white;
   border-width: 10px;
   border-radius: 10px;
-  width: 10%;
+  width: 40%;
+  height: 100px;
   align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+}
+.first:hover{
+  background: darkcyan;
+  width: 42%;
+  height: 110px;
+}
+input{
+  display: flex;
+  height: 30px;
+  margin-bottom: 20px;
+  width: 520px;
+  background: #f2fafa;
+  border-radius: 20px;
+  font-family: 'Kufam', cursive;
+  color: blue;
+  font-size: 20px;
 }
 </style>
 
